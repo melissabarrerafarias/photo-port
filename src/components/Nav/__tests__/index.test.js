f1e865c0cb4239283933217f80aa6574194da423
melissikiss <melissikiss@gmail.com>
2021-02-16 12:03:14 -0800

    ran tests for gallery and photlist + updated test for nav

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -1,36 +1,58 @@
-import React from 'react'; 
-import { render, cleanup } from '@testing-library/react'; 
-import '@testing-library/jest-dom/extend-expect'; 
-import Nav from '..'; 
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Nav from '..';
 
-afterEach(cleanup); 
+const categories = [
+    { name: 'portraits', description: 'Portraits of people in my life' }
+]
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn();
+
+afterEach(cleanup);
 
 describe('Nav component', () => {
     // baseline test 
     it('renders', () => {
-        render(<Nav />); 
-    }); 
+        render(<Nav
+          categories={categories}
+          setCurrentCategory={mockSetCurrentCategory}
+          currentCategory={mockCurrentCategory}
+        />);
+      })
 
     // snapshot test 
     it('matches snapshot', () => {
-        const { asFragment } = render(<Nav />); 
-        expect(asFragment()).toMatchSnapshot(); 
-    }); 
-}); 
+        const { asFragment } = render(<Nav
+            categories={categories}
+            setCurrentCategory={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory}
+          />);
+        expect(asFragment()).toMatchSnapshot();
+    });
+});
 
 // checks if camera emoji is visible 
 describe('emoji is visible', () => {
     it('inserts emoji into h2', () => {
-        const { getByLabelText } = render(<Nav />); // getByLabelText queries aria-label value
-        expect(getByLabelText('camera')).toHaveTextContent('📸'); 
+        const { getByLabelText } = render(<Nav
+            categories={categories}
+            setCurrentCategory={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory}
+          />); // getByLabelText queries aria-label value
+        expect(getByLabelText('camera')).toHaveTextContent('📸');
     })
-}); 
+});
 
 // checks to see if links are visible so users can navigate to parts of app
 describe('links are visible', () => {
     it('inserts text into the links', () => {
-        const { getByTestId } = render(<Nav />); 
-        expect(getByTestId('link')).toHaveTextContent('Oh Snap!'); 
-        expect(getByTestId('about')).toHaveTextContent('About Me'); 
+        const { getByTestId } = render(<Nav
+            categories={categories}
+            setCurrentCategory={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory}
+          />);
+        expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
+        expect(getByTestId('about')).toHaveTextContent('About me');
     })
 }); 
