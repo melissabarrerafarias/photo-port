699a3f4d5cbf328049a02252bd3f14d7f4dc2bf7
melissikiss <melissikiss@gmail.com>
2021-02-16 21:42:00 -0800

    modal closes

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -105,15 +105,17 @@ const PhotoList = ({ category }) => {
   const [currentPhoto, setCurrentPhoto] = useState();
 
   const toggleModal = (image, i) => {
-    setCurrentPhoto({ ...image, index: i })
-    setIsModalOpen(true);
+    setCurrentPhoto({ ...image, index: i });
+    setIsModalOpen(!isModalOpen);
   }
 
   const currentPhotos = photos.filter((photo) => photo.category === category);
 
   return (
     <div>
-      {isModalOpen && <Modal currentPhoto={currentPhoto} />}
+      {isModalOpen && (
+        <Modal currentPhoto={currentPhoto} onClose={toggleModal} />
+      )}
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
           <img
